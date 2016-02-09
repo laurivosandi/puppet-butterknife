064f32319abc3ce8504fb39bbe25addfa1e25914
Lauri Võsandi <lauri.vosandi@gmail.com>
2016-02-09 15:13:01 +0200

    Clean up scripts, reduce Puppetisms

--- manifests/template.pp
+++ manifests/template.pp
@@ -9,27 +9,5 @@ class butterknife::template {
     if $architecture == "i386" or $architecture == "amd64" {
         package { "grub-pc": ensure => installed } ->
         package { "os-prober": ensure => installed }
-        ->
-        file_line { "grub-detect-by-uuid":
-            ensure => present,
-            path => "/etc/default/grub",
-            match => "^#?GRUB_DISABLE_LINUX_UUID=",
-            line => "GRUB_DISABLE_LINUX_UUID=false"
-        }
-    }
-
-    # Make symlink relative on jessie
-    file { "/etc/resolv.conf":
-        ensure => link,
-        target => '../run/resolvconf/resolv.conf'
-    }
-
-    # local time is needed for dual-boot machines,
-    # it's set in post-deploy scripts now.
-    # template shall remain in UTC
-    file_line { "hardware-clock-to-utc":
-        path => "/etc/default/rcS",
-        match => "^UTC=",
-        line => "UTC=yes"
     }
 }
